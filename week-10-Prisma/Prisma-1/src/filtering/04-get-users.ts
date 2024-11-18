9edfbed945a4a6b0d3508651e605b24f0c1567d0
Aman Vishwakarma <123929292+superAman07@users.noreply.github.com>
2024-11-19 02:22:30 +0530

    Add files via upload

---
+++ week-10-Prisma/Prisma-1/src/filtering/04-get-users.ts
@@ -0,0 +1,26 @@
+import { PrismaClient } from "@prisma/client";
+const prisma = new PrismaClient();
+
+async function main(){
+    const users = await prisma.user.findMany({});
+    console.log(users);
+    const user = await prisma.user.findUnique({
+        where:{
+            id:1
+        },
+        include:{
+            posts:true
+        }
+    });
+    console.log(user);
+};
+main()
+.then (async ()=>{
+    console.log("Done");
+    prisma.$disconnect();
+})
+.catch(async (e)=>{
+    console.error(e);
+    await prisma.$disconnect();
+    process.exit(1);
+})
